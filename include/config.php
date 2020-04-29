25f99a8efa8b60a87ff67d04084b91eb263a99ac
sadique-cws <sadique.hussain96@gmail.com>
2020-04-29 20:22:12 +0530

    first commit

---
+++ include/config.php
@@ -0,0 +1,41 @@
+<?php 
+
+$connect = mysqli_connect('localhost','root','','cwsbook') or die("db error");
+
+
+//for session
+session_start();
+
+
+function redirect($page){
+    echo "<script>window.open('$page.php','_self')</script>";
+}
+
+
+//insert function
+function insertData($table,$fields){
+    global $connect;
+    
+    $col = implode(",",array_keys($fields));
+    $row = implode("','",array_values($fields));
+    
+    
+    $query = mysqli_query($connect,"INSERT INTO $table($col) VALUE ('$row')");
+    
+    
+    return ($query)?true:false;
+}
+
+//Count data
+function countData($table,$cond){
+    global  $connect;
+    
+    $query = mysqli_query($connect,"SELECT * FROM $table where $cond");
+    $count = mysqli_num_rows($query);
+    
+    return $count;
+}
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php include_once("include/config.php");?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,17 +15,19 @@
            <a href="index.php" class="navbar-brand mt-2"><h5>CWSbook</h5></a>
        
        
-       <table class="ml-auto">
+       <form method="post">
+           <table class="ml-auto">
            <tr>
                <th class="small text-white">Username/email</th>
                <th class="small text-white">Password</th>
            </tr>
            <tr>
-               <td><input type="text" placeholder="Username/email" class="form-control  form-control-sm"></td>
-               <td><input type="password" placeholder="Password" class="form-control  form-control-sm"></td>
-               <td><input type="submit" class="btn btn-info btn-sm"></td>
+               <td><input type="text" name="username" placeholder="Username/email" class="form-control  form-control-sm"></td>
+               <td><input type="password" name="password" placeholder="Password" class="form-control  form-control-sm"></td>
+               <td><input type="submit" name="login" class="btn btn-info btn-sm"></td>
            </tr>
        </table>
+       </form>
        </div>
    </nav>
     
@@ -38,7 +43,7 @@
                     <div class="card-body">
                        <h2 class="blockquote m-0 p-0">Create an Account</h2>
                        <small class="text-muted">100% free of Cost</small>
-                        <form action="" class="mt-2">
+                        <form method="post" class="mt-2">
                             <div class="row">
                                 <div class="form-group col">
                                 <label for="fname" class="m-0 p-0 text-muted">First Name</label>
@@ -77,3 +82,40 @@
     </div>
 </body>
 </html>
+
+
+<?php
+if(isset($_POST['create_account'])){
+    $data = [
+        'first_name' => $_POST['fname'],
+        'last_name' => $_POST['lname'],
+        'email' => $_POST['email'],
+        'contact' => $_POST['contact'],
+        'dob' => $_POST['dateofbirth'],
+        'password' => md5($_POST['password'])
+    ];
+    
+    if(insertData('account',$data)){
+        redirect('index');
+    }
+    else{
+        echo "fail";
+    }
+}
+
+// login work
+if(isset($_POST['login'])){
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+    
+    $count = countData('account',"(email='$username' OR contact='$username') AND password='$password'");
+    
+    if($count > 0){
+        $_SESSION['user'] = $username;
+        redirect('profile');
+    }
+    else{
+        echo "username and password is incorrect try again";
+    }
+}
+?>
